4c2e090a3d2d8a46bca185c6eaa9ae8be2d36f51
xiaolin1991 <113982716+xiaolin1991@users.noreply.github.com>
2023-09-07 22:03:44 +0800

    Add files via upload

--- src/main/java/cn/jja8/newbinggogo/Config.java
+++ src/main/java/cn/jja8/newbinggogo/Config.java
@@ -1,1 +1,8 @@
-Config.java
+package cn.jja8.newbinggogo;
+
+import cn.jja8.config.tool.Conf;
+
+public class Config {
+    @Conf static boolean joinStats = true;
+    @Conf static String[] cookies = {};
+}
